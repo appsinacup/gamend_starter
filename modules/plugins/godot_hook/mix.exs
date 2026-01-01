86b1338a7c31affaf73f672b012eebb16fbf1462
Dragos Daian <daian.dragos@yahoo.com>
2026-01-01 10:50:46 +0100

    Update godot_hook to 1.0.0

--- modules/plugins/godot_hook/mix.exs
+++ modules/plugins/godot_hook/mix.exs
@@ -4,7 +4,7 @@ defmodule GodotHook.MixProject do
   def project do
     [
       app: :godot_hook,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
