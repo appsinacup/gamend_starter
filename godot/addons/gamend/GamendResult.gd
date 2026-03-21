470a3e4ed49f76bc4ee3c3ac5564b3d8b4c97ea6
Dragos Daian <daian.dragos@yahoo.com>
2026-03-21 09:23:13 +0100

    Remove godot references and move to other branch for now to keep it clean

--- godot/addons/gamend/GamendResult.gd
+++
@@ -1,7 +0,0 @@
-class_name GamendResult
-extends RefCounted
-
-signal finished(result: GamendResult)
-
-var response: ApiApiResponseClient
-var error: ApiApiErrorClient
--- modules/plugins/godot_hook/config/config.exs
+++
@@ -1,4 +0,0 @@
-import Config
-
-# This plugin is configured via app env (`:godot_hook`) and/or environment variables.
-# See README.md for details.
